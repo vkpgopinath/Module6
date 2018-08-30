b563720e9f1d897da62505b2dbcce6ca48d5bfa1
Gopi <vkpgopinath@gmail.com>
2018-08-30 18:00:40 -0300

    Initial commit

---
+++ src/Collection_API.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Collection_API {
+
+	public static void main(String[] args) {
+    
+	ArrayList<String> arrlist = new ArrayList<String>();
+	arrlist.add("Gopi");
+	arrlist.add("Gayathri");
+	arrlist.add("Gopiga");
+	arrlist.add("Grahi");
+	
+    System.out.println(arrlist.get(1));
+    System.out.println(arrlist.size());
+	
+	
+	for(int i=0;i<arrlist.size();i++) {
+		System.out.println(arrlist.get(i));
+	}
+		
+		
+		
+		
+		
+		
+	}
+
+}
